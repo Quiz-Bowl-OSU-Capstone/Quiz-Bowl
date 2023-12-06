b5eead5f751c1c50a2fe801ab023cb6b39a22853
Cloud <33647317+ActuallyCloud@users.noreply.github.com>
2023-12-06 13:20:38 -0800

    Changes to the columns on the table to match the current data

--- Quiz Bowl DB/questions.sql
+++ Quiz Bowl DB/questions.sql
@@ -1,9 +1,13 @@
 CREATE TABLE [dbo].[questions] (
-    [Id]                INT           IDENTITY (1, 1) NOT NULL PRIMARY KEY CLUSTERED ([Id] ASC),
-    [question]          VARCHAR (MAX) NOT NULL,
-    [answers]           VARCHAR (MAX) NOT NULL,
-    [category]          CHAR (64)     NOT NULL,
-    [difficulty]        INT           NOT NULL,
-    [lastusagedate]     DATE          NULL,
-    [lastusagelocation] CHAR (64)     NULL
+    [Id]             INT           IDENTITY (1, 1) NOT NULL PRIMARY KEY CLUSTERED ([Id] ASC),
+    [question]       VARCHAR (MAX) NOT NULL,
+    [answers]        VARCHAR (MAX) NOT NULL,
+    [difficulty]     INT           NOT NULL,
+    [topic]          VARCHAR (50)  NOT NULL,
+    [species]        VARCHAR (50)  NOT NULL,
+    [resource]       VARCHAR (50)  NOT NULL,
+    [pageNum]        INT           NULL,
+    [lastUsageDate]  DATE          NULL,
+    [lastUsageEvent] VARCHAR (50)  NULL,
+    [lastUpdated]    INT           NOT NULL
 );
