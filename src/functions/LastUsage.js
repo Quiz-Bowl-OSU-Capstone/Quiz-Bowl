086465ccd4e4942d79530d84357d6d472f3c601e
jay2002shah <shahj@oregonstate.edu>
2024-02-01 11:34:04 -0800

    added a sql query

--- src/functions/LastUsage.js
+++ src/functions/LastUsage.js
@@ -8,10 +8,30 @@ app.http('LastUsage', {
     handler: async (request, context) => {
         context.log('JavaScript HTTP trigger function processed a request.');
 
-        const name = (req.query.name || (req.body && req.body.name));
-        const responseMessage = name
-            ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        // parse request body to get the question , place, and datetime
+        const {question, date, time} = request.body;
+        const placeDateTime = `${date} - ${time}`;
+
+        //connect to the sql server
+        await sql.connect(connString);
+
+        // define a sql query to update the last usage place and datetime for the question
+        const query = `
+        UPDATE QuizQuestions
+        SET Usage_date_time = @placeDateTime
+        WHERE Question = @question`;
+
+        const result = await sql.query(query, {
+            question: question,
+            placeDateTime: placeDateTime
+        });
+
+        await sql.close();
+        
+        // const name = (req.query.name || (req.body && req.body.name));
+        // const responseMessage = name
+        //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
         context.res = {
             // status: 200, /* Defaults to 200 */
