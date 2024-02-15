9e07dfaefa55763ab6c1a752af5ec0284792b0f5
Cloud <33647317+ActuallyCloud@users.noreply.github.com>
2024-02-15 09:40:48 -0800

    Added a function to check if an account is valid and return its details if so.

---
+++ src/functions/ValidAccount.js
@@ -0,0 +1,28 @@
+const { app } = require('@azure/functions');
+const sql = require('mssql');
+const connString = process.env.dbconn;
+
+// Will check if an account exists in the database and return the account details if it does, or return an error if not.
+
+// URL Parameters:
+// - username: The username of the account.
+// - password: The password for the account.
+
+app.http('ValidAccount', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        const pool = await sql.connect(connString);
+        const user = request.query.get('username');
+        const pass = request.query.get('password');
+        const query = "SELECT * FROM [dbo].[Accounts] WHERE username=N'" + user + "' AND password=N'" + pass + "'"
+
+        const data = await pool.request().query(query);
+
+        if (data.recordset.length > 0) {
+            return { body: JSON.stringify(data.recordset[0]) };
+        } else {
+            return { body: `No account found!` };
+        }
+    }
+});
