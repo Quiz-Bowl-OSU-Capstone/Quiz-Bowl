97a4aa2a55a2bf45de4146374c8980a29ab203c2
Aura <33647317+ActuallyCloud@users.noreply.github.com>
2024-02-22 12:58:41 -0800

    Fixed output

--- src/functions/SearchFilters.js
+++ src/functions/SearchFilters.js
@@ -19,50 +19,34 @@ app.http('SearchFilters', {
         currentFilters = Object.create(filters);
         for (i = 0; i < data.recordset.length; i++) {
             if (currentFilters.species.indexOf(data.recordset[i].Species) < 0) {
-                currentFilters.species.push(data.recordset[i].Species);
+                if (data.recordset[i].Species != "" && data.recordset[i].Species != null) {
+                    currentFilters.species.push(data.recordset[i].Species);
+                }
             }
 
             if (currentFilters.resource.indexOf(data.recordset[i].Resource) < 0) {
-                currentFilters.resource.push(data.recordset[i].Resource);
+                if (data.recordset[i].Resource != "" && data.recordset[i].Resource != null) {
+                    currentFilters.resource.push(data.recordset[i].Resource);
+                }
             }
 
             if (currentFilters.level.indexOf(data.recordset[i].Level) < 0) {
-                currentFilters.level.push(data.recordset[i].Level);
+                if (data.recordset[i].Level != "" && data.recordset[i].Level != null) {
+                    currentFilters.level.push(data.recordset[i].Level);
+                }
             }
 
             if (currentFilters.topic.indexOf(data.recordset[i].Topic) < 0) {
-                currentFilters.topic.push(data.recordset[i].Topic);
+                if (data.recordset[i].Topic != "" && data.recordset[i].Topic != null) {
+                    currentFilters.topic.push(data.recordset[i].Topic);
+                }
             }
         }
 
-        for (j = 0; j < currentFilters.species.length; j++) {
-            if (currentFilters.species[j] == "" || currentFilters.species[j] == null) {
-                currentFilters.species.splice(j, 1);
-            }
-        }
+        resString = "{[" + currentFilters.species + "], [" + currentFilters.resource + "], [" + currentFilters.level + "], [" + currentFilters.topic + "]"
 
-        for (j = 0; j < currentFilters.resource.length; j++) {
-            if (currentFilters.resource[j] == "" || currentFilters.resource[j] == null) {
-                currentFilters.resource.splice(j, 1);
-            }
-        }
-
-        for (j = 0; j < currentFilters.level.length; j++) {
-            if (currentFilters.level[j] == "" || currentFilters.level[j] == null) {
-                currentFilters.level.splice(j, 1);
-            }
-        }
-
-        for (j = 0; j < currentFilters.topic.length; j++) {
-            if (currentFilters.topic[j] == "" || currentFilters.topic[j] == null) {
-                currentFilters.topic.splice(j, 1);
-            }
-        }
-
-
-        var resString = JSON.parse("[" + JSON.stringify(currentFilters.species) + "," + JSON.stringify(currentFilters.resource) + "," + JSON.stringify(currentFilters.level) + "," + JSON.stringify(currentFilters.topic) + "]");
-        console.log(resString);
-    
-        return { body: resString} ;
+        return { body: resString, headers: {
+            'Content-Type': 'application/json'
+        }} ;
     }
 });
